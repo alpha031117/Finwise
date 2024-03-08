da2be6a63bdde10896806b0e05ca4fc02857ce8d
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2024-03-09 00:03:37 +0800

    model pushed

---
+++ lib/model/article.dart
@@ -0,0 +1,15 @@
+class Article {
+  String title;
+  String subtitle;
+  DateTime date;
+  String author;
+  String text;
+
+  Article({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.author,
+    required this.text,
+  });
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class MyUser {
+  String username;
+  String email;
+  String password;
+  String occupation;
+  int points;
+
+  MyUser({
+    required this.username,
+    required this.email,
+    required this.password,
+    required this.occupation,
+    required this.points,
+  });
+}
