fbf5900aab19590e7642b37abbd49065f271e5f5
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2024-03-09 00:01:37 +0800

    update new users

---
+++ lib/data/users.dart
@@ -0,0 +1,29 @@
+import 'package:vhack_finwise_app/model/user.dart';
+
+class UserDatabase {
+  static List<MyUser> users = [
+    MyUser(
+        username: 'doodledaron',
+        email: 'doodledaron@gmail.com',
+        password: 'password123',
+        points: 400, occupation: 'Student',),
+    MyUser(
+        username: 'alan', email: 'testing@gmail.com', password: 'testing111', points: 500, occupation: 'Educator'),
+    MyUser(
+        username: 'alphaaaa', email: 'testing@gmail.com', password: 'testing111', points: 6969, occupation: 'Business owner'),
+    MyUser(
+        username: 'songsong', email: 'testing@gmail.com', password: 'testing111', points: 666, occupation: 'Business owner'),
+    MyUser(
+        username: 'beebee', email: 'testing@gmail.com', password: 'testing111', points: 999, occupation: 'Business owner'),
+    MyUser(
+        username: 'alexchong', email: 'testing@gmail.com', password: 'testing111', points: 30, occupation: 'Business owner'),
+    MyUser(
+        username: 'truegod', email: 'testing@gmail.com', password: 'testing111', points: 50, occupation: 'Business owner'),
+    MyUser(
+        username: 'ben alan', email: 'testing@gmail.com', password: 'testing111', points: 20, occupation: 'Designer'),
+    MyUser(
+        username: 'Leon loo', email: 'testing@gmail.com', password: 'testing111', points: 100, occupation: 'Engineer'),
+    MyUser(
+        username: 'doodoo', email: 'testing@gmail.com', password: 'testing111', points: 300, occupation: 'Student'),
+  ];
+}
