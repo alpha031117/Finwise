b1da7996777da99e465a9a570aedcae1d5cf8098
Alan Koo <alankoo0515@gmail.com>
2024-03-09 02:15:50 +0800

    AppBar Created

--- lib/screens/settings/ui/settings_screen.dart
+++ lib/screens/settings/ui/settings_screen.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vhack_finwise_app/utils/global_variables.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -6,8 +11,29 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('Settings Screen'),
-    ));
+      //   body: Center(
+      // child: Text('Settings Screen'),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Profile',
+            style: TextStyle(
+                fontFamily: GlobalVariables.titleFont().fontFamily,
+                fontWeight: FontWeight.bold,
+                color: GlobalVariables.tertiaryColor,
+                letterSpacing: 1
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Setting Screen',
+          style: TextStyle(
+            fontFamily: GlobalVariables.pointFont().fontFamily,
+          ),
+        ),
+      ),
+    );
   }
 }
